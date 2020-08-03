4fcd307b9231c02beeac6e7369fe746dd3a76786
Pranav <gautam.pranav09@gmail.com>
2020-08-03 22:05:11 +0530

    No Change

--- test_Flask.py
+++ test_Flask.py
@@ -3,14 +3,36 @@ from flask import Flask,render_template
 app = Flask(__name__) 
 
 
+posts=[
+	{
+		'author':'Pranav',
+		'title':'Blog Dummy',
+		'content':'First Post',
+		'date':'Aug-02',
+
+
+	}
+	,
+	{
+		'author':'Duke',
+		'title':'Blog Dummy 2',
+		'content':'Second Post',
+		'date':'Aug-04'
+
+
+	}
+	
+]
+
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')  
+    return render_template('home.html',posts=posts,title='HOME')  
 
 @app.route('/about')
 def about():
-	return  render_template('about.html')
+	return  render_template('about.html',title='ABOUT')
 
 if __name__ == '__main__':
 	app.run(debug=True)
